cf7c8769fb8d245b150e3ce91716228f7c721c8a
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-05-07 19:25:48 +0300

    create pages title and url classes

---
+++ src/main/java/com/rabbit_farm/configurations/address/PagesTitle.java
@@ -0,0 +1,8 @@
+package com.rabbit_farm.configurations.address;
+
+public class PagesTitle {
+	public static final String LOGIN_PAGE = "Login page";
+	public static final String REGISTER_PAGE = "Register page";
+	public static final String USER_EDIT_PAGE = "Edit user";
+	public static final String USER_INFO_PAGE = "User info";
+}
---
+++ src/main/java/com/rabbit_farm/configurations/address/PagesUrl.java
@@ -0,0 +1,25 @@
+package com.rabbit_farm.configurations.address;
+
+public class PagesUrl {
+	public static final String HOME_PAGE = "/";
+	public static final String HOME_PAGE_TEXT = "Home";
+
+	public static final String LOGIN_PAGE = "/user/login";
+	public static final String LOGIN_PAGE_TEXT = "Login";
+
+	public static final String REGISTER_PAGE = "/user/register";
+	public static final String REGISTER_PAGE_TEXT = "Register";
+
+	public static final String LOGOUT_PAGE = "/user/logout";
+	public static final String LOGOUT_PAGE_TEXT = "Logout";
+
+	public static final String USER_INFO_PAGE = "/user/info";
+	public static final String USER_INFO_PAGE_TEXT = "Info";
+
+	public static final String USER_EDIT_PAGE = "/user/edit";
+	public static final String USER_EDIT_PAGE_TEXT = "Edit";
+
+	public static final String USER_DELETE_PAGE = "/user/edit";
+	public static final String USER_DELETE_PAGE_TEXT = "Edit";
+
+}
