3f65d6966eaf5a87376a1cf866654d6f0faf12e7
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-05-07 19:20:27 +0300

    create user exceptions

---
+++ src/main/java/com/rabbit_farm/areas/user/exceptions/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rabbit_farm.areas.user.exceptions;
+
+import com.rabbit_farm.configurations.errors.UserErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = UserErrors.NOT_FOUND_USER)
+public class UserNotFoundException extends UserException {
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/rabbit_farm/areas/user/exceptions/UsersDbEmptyException.java
@@ -0,0 +1,12 @@
+package com.rabbit_farm.areas.user.exceptions;
+
+import com.rabbit_farm.configurations.errors.UserErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = UserErrors.DATABASE_IS_EMPTY)
+public class UsersDbEmptyException extends UserException {
+	public UsersDbEmptyException(String message) {
+		super(message);
+	}
+}
