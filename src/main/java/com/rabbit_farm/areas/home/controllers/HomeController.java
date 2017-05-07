05f860ebce63a12187bb1e93b5458f89e48f33c9
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-05-07 19:25:26 +0300

    create home controller and home html page

--- src/main/java/com/rabbit_farm/areas/home/controllers/HomeController.java
+++ src/main/java/com/rabbit_farm/areas/home/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 	@GetMapping("/")
-	public String redirectToRabbitCreatePage() {
-		return "redirect:/rabbit/create";
+	public String redirectToRabbitShowAllPage() {
+		return "redirect:/rabbit/show/all";
 	}
 }
