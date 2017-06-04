2e0747874b94ba1359d6f9e18eab49818c772a45
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-06-04 21:40:37 +0300

    create rabbit error

---
+++ src/main/java/com/rabbit_farm/areas/rabbit/errors/RabbitErrors.java
@@ -0,0 +1,7 @@
+package com.rabbit_farm.areas.rabbit.errors;
+
+public class RabbitErrors {
+	public static final String INVALID_RABBIT = "Rabbit is not valid!";
+	public static final String EMPTY_DATABASE = "Rabbit database is empty!";
+	public static final String NOT_FOUND = "Rabbit not found in database!";
+}
