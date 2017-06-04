498930ee19a7800fbf98e0cc8a420452a3c96604
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-06-04 18:30:41 +0300

    refactoring

--- src/main/java/com/rabbit_farm/configurations/address/PagesTitle.java
+++ src/main/java/com/rabbit_farm/configurations/address/PagesTitle.java
@@ -1,8 +1,12 @@
 package com.rabbit_farm.configurations.address;
 
 public class PagesTitle {
-	public static final String LOGIN_PAGE = "Login page";
-	public static final String REGISTER_PAGE = "Register page";
+	//User
+	public static final String USER_LOGIN_PAGE = "Login page";
+	public static final String USER_REGISTER_PAGE = "Register page";
 	public static final String USER_EDIT_PAGE = "Edit user";
 	public static final String USER_INFO_PAGE = "User info";
+
+	//Rabbit
+	public static final String RABBIT_ALL = "All rabbits";
 }
--- src/main/java/com/rabbit_farm/configurations/address/PagesUrl.java
+++ src/main/java/com/rabbit_farm/configurations/address/PagesUrl.java
@@ -4,6 +4,7 @@ public class PagesUrl {
 	public static final String HOME_PAGE = "/";
 	public static final String HOME_PAGE_TEXT = "Home";
 
+	//User
 	public static final String LOGIN_PAGE = "/user/login";
 	public static final String LOGIN_PAGE_TEXT = "Login";
 
@@ -19,7 +20,15 @@ public class PagesUrl {
 	public static final String USER_EDIT_PAGE = "/user/edit";
 	public static final String USER_EDIT_PAGE_TEXT = "Edit";
 
-	public static final String USER_DELETE_PAGE = "/user/edit";
-	public static final String USER_DELETE_PAGE_TEXT = "Edit";
+	public static final String USER_DELETE_PAGE = "/user/delete";
+	public static final String USER_DELETE_PAGE_TEXT = "Delete";
+
+	//Rabbit
+	public static final String RABBIT_SHOW_ALL = "/rabbit/show/all";
+	public static final String RABBIT_SHOW_ALL_TEXT = "All rabbit";
+
+	public static final String RABBIT_ADD = "/rabbit/add";
+	public static final String RABBIT_ADD_TEXT = "Add rabbit";
+
 
 }
