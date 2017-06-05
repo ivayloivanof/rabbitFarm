405801abfa21b792007daaed167a13b21fea0981
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2017-06-05 21:52:32 +0300

    create show all rabbit page, fragments and path in controller with implementation

--- src/main/java/com/rabbit_farm/areas/rabbit/controllers/RabbitController.java
+++ src/main/java/com/rabbit_farm/areas/rabbit/controllers/RabbitController.java
@@ -1,5 +1,9 @@
 package com.rabbit_farm.areas.rabbit.controllers;
 
+import com.rabbit_farm.areas.rabbit.services.RabbitService;
+import com.rabbit_farm.configurations.address.PagesTitle;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/rabbit")
 public class RabbitController {
+
+	private RabbitService rabbitService;
+
+	@GetMapping("/show/all")
+	public String getRabbitShowPage(Model model) {
+		model.addAttribute("title", PagesTitle.RABBIT_ALL);
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		return "rabbit/show";
+	}
 
 	@GetMapping("/create")
 	public String getRabbitCreatePage(Model model) {
